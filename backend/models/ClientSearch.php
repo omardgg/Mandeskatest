7eb0f7f0ae9312cd8e5289aa17118144aea4e634
Omar <omardgg_26@outlook.com>
2023-08-14 21:26:33 -0400

    fix minor changes

--- backend/models/ClientSearch.php
+++ backend/models/ClientSearch.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace app\models;
+namespace backend\models;
 
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use app\models\Client;
+use backend\models\Client;
 
 /**
  * ClientSearch represents the model behind the search form of `app\models\Client`.
