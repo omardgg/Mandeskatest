8656bb2166dd604c638878a9156d2bdf8b11b0b9
Omar <omardgg_26@outlook.com>
2023-08-17 12:50:56 -0400

    minorchanges

--- backend/config/main.php
+++ backend/config/main.php
@@ -14,7 +14,8 @@ return [
     'modules' => [],
     'components' => [
         'request' => [
-            'csrfParam' => '_csrf-backend',
+            'class' => 'yii\web\Request',
+            'enableCsrfValidation' => false,
         ],
         'user' => [
             'identityClass' => 'common\models\User',
